911abfcfcf2c0261b1e230f989ab07663c42c446
MEHUL <murtimehul256@gmail.com>
2025-03-31 11:22:01 +0530

    Create, Delete and Read Notes view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
-    path("api-aut/", include("rest_framework.urls"))
+    path("api-aut/", include("rest_framework.urls")),
+    path("api/", include("api.urls")),
 ]
